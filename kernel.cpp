388304906896def183cea09387e1bc9a94f7002a
chndr1 <bhargav.annem@gmail.com>
2022-10-14 18:11:10 -0500

    ports

--- kernel.cpp
+++ kernel.cpp
@@ -1,8 +1,34 @@
 #include "types.h"
+#include "gdt.h"
 
-void printf(char* str){ uint16_t* video_memory = (uint16_t*) 0xb8000;
+void printf(char* str){
+  uint16_t* video_memory = (uint16_t*) 0xb8000;
+  static uint8_t x = 0, y = 0;
   for(int i = 0; str[i] != '\0'; i++){
-    video_memory[i] = (video_memory[i] & 0xFF00) | str[i];
+    switch (str[i])
+    {
+      case '\n':
+          y++;
+          x=0;
+        break;
+      default:
+        video_memory[80 * y + x] = (video_memory[80 * y + x] & 0xFF00) | str[i];
+        x++;
+        break;
+    }
+    if(x >= 80){
+      y++;
+      x=0;
+    }
+    if(y >= 25){
+      for(y = 0; y < 25; y++){
+        for(x = 0; x < 80; x++){
+          video_memory[80 * y + x] = (video_memory[80 * y + x] & 0xFF00) | ' ';
+        }
+      }
+      y = 0;
+      x = 0;
+    }
   }
 }
 
@@ -18,7 +44,8 @@ extern "C" void call_constructors() {
 
 extern "C" void kernel_main(void* multiboot_struct, uint32_t magic_number) {
 
-  printf("THIS WORKS");
-
+  printf("THIS WORKS\n");
+  printf("THIS WORKS\n");
+  GlobalDescriptorTable gdt;
   while(1);
 }
